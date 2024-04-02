fbe84e27e7c085fafde3899a6db823ee79babe79
Pavithra <pavithra.njn@gmail.com>
2024-04-02 14:58:21 -0700

    Update pubmed_parser.py

--- pubmed_parser.py
+++ pubmed_parser.py
@@ -8,28 +8,31 @@ def parse_pubmed(ids):
        ids: .csv file [NO HEADER] with a list of PMIDs of interest 1 on each line.
     
     Output:
-       .csv file with the following columns: 
-          article
-          title
-          year
+       tblReferences.csv file with the following columns: 
+          RefID
+          Author
+          Title
+          Journal
+          RefYear
+          MedlineID
           first author last name
           first author first name initial
           first author initials
+        tblAuthors.csv file with the following columns: 
+          RefID
+          LastName
+          Initials
     '''
     
-    ids=pd.read_csv(ids,header=None)
-    input_data=''
-    for v in ids[0].values:
-        input_data+=str(v)+','
-    input_data=input_data[0:-1]
+    ids=pd.read_csv(ids,header=None)[0].values
     
-    
-    output_data={'title':[],'year':[],'lastname':[],'first_initial':[],'initials':[]}
+    output_data={'title':[],'year':[],'lastname':[],'first_initial':[],'initials':[],'id':[],'journal':[]}
     for entry in input_data.split(','):
         fetch = PubMedFetcher()
         article = fetch.article_by_pmid(entry)
         title=article.title
         year=article.year
+        journal=article.journal
         lastname=article.author1_last_fm.split(' ')[0]
         first_initial=article.author1_last_fm.split(' ')[1]
         initials=first_initial + lastname[0]
@@ -38,10 +41,22 @@ def parse_pubmed(ids):
         output_data['year'].append(year)
         output_data['lastname'].append(lastname)
         output_data['first_initial'].append(first_initial)
+        output_data['journal'].append(journal)
         output_data['initials'].append(initials)
+        output_data['id'].append(entry)
             
-    pd.DataFrame(output_data).to_csv('meta_data.csv',index=False)
+    res=pd.DataFrame(output_data)
+    res['RefID']=None
 
+    authors=res[['RefID','lastname','initials']]
+    authors.columns=['RefID','LastName','Initials']
+    authors.to_csv('tblAuthors.csv',index=False)
+
+    res['Author']=res['lastname']+', '+res['first_initial']
+    res=res[['RefID','Author', 'title','journal','year','id']]
+    res.columns=['RefID','Author','Title','Journal','RefYear','MedlineID']
+    res['Published']='Yes'
+    res.to_csv('tblReferences.csv',index=False)
 
 def main():
     parser = argparse.ArgumentParser("PUBMED METADATA PARSER")
