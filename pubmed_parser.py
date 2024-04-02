fa97bb560c40ac4a6e3390c4418eae9403a9d67e
Pavithra <pavithra.njn@gmail.com>
2024-04-02 15:32:09 -0700

    Update pubmed_parser.py

--- pubmed_parser.py
+++ pubmed_parser.py
@@ -9,7 +9,7 @@ def parse_pubmed(ids):
     
     Output:
        tblReferences.csv file with the following columns: 
-          RefID
+          _RefID
           Author
           Title
           Journal
@@ -19,7 +19,7 @@ def parse_pubmed(ids):
           first author first name initial
           first author initials
         tblAuthors.csv file with the following columns: 
-          RefID
+          _RefID
           LastName
           Initials
     '''
@@ -27,6 +27,8 @@ def parse_pubmed(ids):
     ids=pd.read_csv(ids,header=None)[0].values
     
     output_data={'title':[],'year':[],'lastname':[],'first_initial':[],'initials':[],'id':[],'journal':[]}
+    authors={'_RefID':[],'LastName':[],'Initials':[]} #RefID    LastName    Initials
+    counter=0
     for entry in ids:
         fetch = PubMedFetcher()
         article = fetch.article_by_pmid(entry)
@@ -44,19 +46,22 @@ def parse_pubmed(ids):
         output_data['journal'].append(journal)
         output_data['initials'].append(initials)
         output_data['id'].append(entry)
+    for a in article.authors:
+            authors['_RefID'].append(counter)
+            authors['LastName'].append(a.split(' ')[0])
+            authors['Initials'].append(a.split(' ')[1]+a.split(' ')[0][0])
+        counter+=1
             
     res=pd.DataFrame(output_data)
-    res['RefID']=None
-
-    authors=res[['RefID','lastname','initials']]
-    authors.columns=['RefID','LastName','Initials']
-    authors.to_csv('tblAuthors.csv',index=False)
-
+    res['_RefID']=[i for i in range(len(res))]
     res['Author']=res['lastname']+', '+res['first_initial']
-    res=res[['RefID','Author', 'title','journal','year','id']]
-    res.columns=['RefID','Author','Title','Journal','RefYear','MedlineID']
+    res=res[['_RefID','Author', 'title','journal','year','id']]
+    res.columns=['_RefID','Author','Title','Journal','RefYear','MedlineID']
     res['Published']='Yes'
     res.to_csv('tblReferences.csv',index=False)
+
+    res2=pd.DataFrame(authors)
+    res2.to_csv('tblAuthors.csv',index=False)
 
 def main():
     parser = argparse.ArgumentParser("PUBMED METADATA PARSER")
